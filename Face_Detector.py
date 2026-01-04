1859c5de1f4215fe3403c948b7d2bc73ae7b0cef
John786-tech <jahaangauri@gmail.com>
2026-01-04 14:44:39 +0530

    Update Face_Detector.py

--- Face_Detector.py
+++ Face_Detector.py
@@ -1,24 +1,39 @@
 import cv2
+import matplotlib.pyplot as plt
+from IPython.display import display, clear_output
 
-face_cap = cv2.CascadeClassifier("C:/Users/jahaa/AppData/Local/Programs/Python/Python311/Lib/site-packages/cv2/data/haarcascade_frontalface_default.xml")
+face_cap = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
+
 video_cap = cv2.VideoCapture(0)
-while True:
-    ret, video_data = video_cap.read()
-    if not ret:
-        break        
-    col = cv2.cvtColor(video_data, cv2.COLOR_BGR2GRAY)
-    faces = face_cap.detectMultiScale(
-        col,
-        scaleFactor=1.1,
-        minNeighbors=5,
-        minSize=(30, 30),
-        flags=cv2.CASCADE_SCALE_IMAGE
-    )
-    for (x, y, w, h) in faces:
-        cv2.rectangle(video_data, (x, y), (x+w, y+h), (0, 255, 0), 2)
-    cv2.imshow("video_Live", video_data)
-    if cv2.waitKey(10) == ord("a"):
-        break
+
+try:
+    while True:
+        ret, video_data = video_cap.read()
+        if not ret:
+            break
+            
+        frame_rgb = cv2.cvtColor(video_data, cv2.COLOR_BGR2RGB)
         
+        faces = face_cap.detectMultiScale(
+            frame_rgb,
+            scaleFactor=1.1,
+            minNeighbors=5,
+            minSize=(30, 30),
+            flags=cv2.CASCADE_SCALE_IMAGE
+        )
+
+        for (x, y, w, h) in faces:
+            cv2.rectangle(frame_rgb, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
+        plt.figure(figsize=(10, 6))
+        plt.imshow(frame_rgb)
+        plt.axis('off')
+        plt.show()
+        
+        clear_output(wait=True)
+
+except KeyboardInterrupt:
+    video_cap.release()
+    print("Stream stopped")
+    
 video_cap.release()
-cv2.destroyAllWindows()
